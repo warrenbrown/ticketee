62c5f0e9b22d5724d04d38169ff4eb560c75d4d4
warrenbrown <warren@katapultmedia.com>
2016-04-26 10:40:30 -0500

    Redirect users back to projects page if they try going to a project that does not exits

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+before_action :set_project, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @projects = Project.all
   end
@@ -20,15 +22,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find_by(params[:id])
   end
 
   def update
-    @project = Project.find_by(params[:id])
     if @project.update(project_params)
       flash[:notice] = 'Project has been updated.'
       redirect_to @project
@@ -39,13 +38,18 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find_by(params[:id])
-
     @project.destroy
     flash[:notice] = 'Project has been deleted.'
     redirect_to projects_path
   end
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'The project you are looking for could not be found.'
+    redirect_to projects_path
+  end
 
   def project_params
     params.require(:project).permit(:name, :description)
