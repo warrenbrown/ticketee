216a029ae7dea667baeb422dfee2f99764f289f7
warrenbrown <warren@katapultmedia.com>
2016-05-06 11:42:00 -0500

    Add admin namespace with application controller

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,14 @@
+class Admin::ApplicationController < ApplicationController
+  before_action :authorize_admin!, :authenticate_user!
+  def index
+  end
+
+  private
+
+  def authorize_admin!
+    authenticate_user!
+    unless current_user.admin?
+      redirect_to root_path, alert: 'You must be admin to do that.'
+    end
+  end
+end
---
+++ spec/views/admin/application/index.html.erb_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe "application/index.html.erb", type: :view do
+end
