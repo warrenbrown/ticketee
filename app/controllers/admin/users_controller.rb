cc48470f27770febb669a6fce33abc71ad32eae0
warrenbrown <warren@katapultmedia.com>
2016-05-07 12:01:25 -0500

    Add ability for admins to edit and update

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,8 +19,32 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+    end
+
+    if @user.update(user_params)
+      flash[ :notice ] = 'User has been updated.'
+      redirect_to admin_users_path
+    else
+      flash.now[ :alert ] = 'User has not been updated.'
+      render 'edit'
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :admin)
   end
 end
--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -16,4 +16,14 @@ RSpec.feature 'Admins can create users' do
 
     expect(page).to have_content 'User has been saved.'
   end
+
+  scenario 'when the new user is an admin' do
+    fill_in 'Email', with: 'shadow@example.com'
+    fill_in 'Password', with: 'password'
+    check 'Is an admin?'
+    click_button 'Create User'
+
+    expect(page).to have_content 'User has been saved.'
+    expect(page).to have_content 'shadow@example.com (Admin)'
+  end
 end
