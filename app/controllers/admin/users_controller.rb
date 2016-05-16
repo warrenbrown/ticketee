171685cf7a054a9fd15a3a1c2e21efb8212f6311
warrenbrown <warren@katapultmedia.com>
2016-05-16 10:29:54 -0500

    Admins can edit project roles of eixisting projects

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,8 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_projects, only: [ :new, :create, :update, :edit ]
+
   skip_after_action :verify_authorized, :verify_policy_scoped
-  
+
   before_action :set_user, only: [:show, :edit, :update, :archive]
 
   def index
@@ -38,12 +40,23 @@ class Admin::UsersController < ApplicationController
       params[:user].delete(:password)
     end
 
-    if @user.update(user_params)
-      flash[ :notice ] = 'User has been updated.'
-      redirect_to admin_users_path
-    else
-      flash.now[ :alert ] = 'User has not been updated.'
-      render 'edit'
+    User.transaction do
+      @user.roles.clear
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+        if role_name.present?
+          @user.roles.build(project_id: project_id, role: role_name)
+        end
+      end
+
+      if @user.update(user_params)
+        flash[ :notice ] = 'User has been updated.'
+        redirect_to admin_users_path
+      else
+        flash.now[ :alert ] = 'User has not been updated.'
+        render 'edit'
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
@@ -58,6 +71,10 @@ class Admin::UsersController < ApplicationController
   end
 
   private
+  def set_projects
+    @projects = Project.order(:name)
+  end
+
   def set_user
     @user = User.find(params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :roles
   has_many :tickets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -21,5 +22,9 @@ class User < ActiveRecord::Base
 
   def inactive_message
     archived_at.nil? ? super : :archived
+  end
+
+  def role_on(project)
+    roles.find_by(project_id: project).try(:name)
   end
 end
